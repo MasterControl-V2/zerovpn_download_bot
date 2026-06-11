ca9c09b02a04f278251224302a5e1fb1aabdc39d
MasterControl-V2 <mycontrol.bot2@gmail.com>
2026-06-12 03:05:09 +0700

    Create constants.js

---
+++ functions/constants.js
@@ -0,0 +1,23 @@
+// constants.js
+// Download Bot Only - No Link Checker, No Group Management
+
+export const TELEGRAM_API = "https://api.telegram.org/bot";
+
+// Bot Commands only for Downloaders
+export const BOT_COMMANDS = [
+    '/start',
+    '/fb',
+    '/fbdl',
+    '/tik',
+    '/tiktok',
+    '/yt',
+    '/youtube',
+    '/song',
+    '/audio',
+    '/tx'
+];
+
+// For validation (optional, can be empty)
+export const ADMIN_USERNAME = "@Zero_Free_Vpn";
+export const SUPPORT_GROUP_LINK = "https://t.me/zero_freevpn";
+export const OWNER_ADMIN_IDS = [7576434717, 7240495054];
